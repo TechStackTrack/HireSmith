3d09076753dd1b3ae5f7b3a2d476023bc5ddd329
Bianca Picasso <bianca@Biancas-MacBook-Pro-2.local>
2022-06-22 10:37:40 -0700

    add question and getmoreInfo on question middleware and router
    Co-authored-by: Avi Kerson <avitacos@gmail.com>
    Co-authored-by: Bianca Picasso <bianca.picasso@gmail.com>
    Co-authored-by: Chris LeBrett <100822842+fscgolden@users.noreply.github.com>
    Co-authored-by: Huy Pham <huypham048@gmail.com>
    Co-authored-by: Sophia Chiao <37905400+ssdrsv@users.noreply.github.com>

--- src/server/routes/router.js
+++ src/server/routes/router.js
@@ -7,9 +7,15 @@ router.get('/questions', dbController.getQuestions, (req, res) => {
   return res.status(200).json(res.locals.questions);
 });
 
-router.post('/questions', dbController.addQuestion, (req, res) => {
-  return res.status(200).json(res.locals.questions);
+router.post('/questions', dbController.findQuestion, dbController.addQuestion, dbController.findCompany, dbController.addCompany, dbController.findRound, dbController.addRound, dbController.addToJoin, (req, res) => {
+  return res.status(200).json(res.locals);
 })
+
+
+router.get('/:questionid', dbController.getMoreInfo, (req, res) => {
+  console.log('from router', res.locals.moreInfo)
+  return res.status(200).json(res.locals.moreInfo);
+});
 
 // router.post('/questions', dbController.createQuestion, (req, res) => {
 //   return res.status(200).json(res.locals)
